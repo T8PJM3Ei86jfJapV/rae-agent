3d6bd7d6fd75182e8edbe4f8e0b2affc6fb3b690
ricoxie <dev@ricoxie.com>
2016-04-16 02:18:44 +0800

    Update test.py

--- test.py
+++ test.py
@@ -5,7 +5,6 @@ import requests
 # ----------------------------    1、拉取程序包    -------------------------
 instruct = {
     'agent_name'  : 'hello',
-    'service_port': '8080',
     'agent_action': 'fetch',
     'package_link': 'http://static.ricoxie.com/tmp/hello-v1.0.0.zip'
 }
@@ -17,7 +16,6 @@ instruct = {
     'agent_name'  : 'hello',
     'service_port': '8080',
     'agent_action': 'start',
-    'package_link': 'http://static.ricoxie.com/tmp/hello-v1.0.0.zip'
 }
 print requests.post("http://192.168.229.135:15100/async", data=instruct).text
 # > {"code": "200", "msg": ""}
@@ -27,7 +25,6 @@ instruct = {
     'agent_name'  : 'hello',
     'service_port': '8080',
     'agent_action': 'restart',
-    'package_link': 'http://static.ricoxie.com/tmp/hello-v1.0.0.zip'
 }
 print requests.post("http://192.168.229.135:15100/async", data=instruct).text
 # > {"code": "200", "msg": ""}
@@ -35,9 +32,7 @@ print requests.post("http://192.168.229.135:15100/async", data=instruct).text
 # ---------------------------    4、查看活跃状态    -------------------------
 instruct = {
     'agent_name'  : 'hello',
-    'service_port': '8080',
     'agent_action': 'alive',
-    'package_link': 'http://static.ricoxie.com/tmp/hello-v1.0.0.zip'
 }
 print requests.post("http://192.168.229.135:15100/sync", data=instruct).text
 # > {"code": "200", "msg": "All well!"}
@@ -45,9 +40,7 @@ print requests.post("http://192.168.229.135:15100/sync", data=instruct).text
 # ------------------------------      5、停止      ---------------------------
 instruct = {
     'agent_name'  : 'hello',
-    'service_port': '8080',
     'agent_action': 'stop',
-    'package_link': 'http://static.ricoxie.com/tmp/hello-v1.0.0.zip'
 }
 print requests.post("http://192.168.229.135:15100/async", data=instruct).text
 # > {"code": "200", "msg": ""}
@@ -55,9 +48,7 @@ print requests.post("http://192.168.229.135:15100/async", data=instruct).text
 # --------------------------     6、查看活跃状态    --------------------------
 instruct = {
     'agent_name'  : 'hello',
-    'service_port': '8080',
     'agent_action': 'alive',
-    'package_link': 'http://static.ricoxie.com/tmp/hello-v1.0.0.zip'
 }
 print requests.post("http://192.168.229.135:15100/sync", data=instruct).text
 # > {"code": "300", "msg": "Something bad!"}
